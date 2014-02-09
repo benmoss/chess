dee9813bb7bb53f6c9011ca244b259c7a3db8749
Ben Moss <ben@mossity.com>
2014-02-09 14:51:11 -0500

    change available-moves to multimethod

--- src/cljs/forking_chess/piece.cljs
+++ src/cljs/forking_chess/piece.cljs
@@ -1,6 +1,8 @@
 (ns forking-chess.piece
   (:require [clojure.set :refer [map-invert]]))
 
+(def board-size 8)
+
 (def column-to-int
   (zipmap (seq "abcdefgh")
           (range)))
@@ -8,22 +10,25 @@
 (def int-to-column
   (map-invert column-to-int))
 
+
 (defn position-to-coords [position]
   {:x (-> position first column-to-int)
    :y (-> position last str js/parseInt dec)})
 
-(defn coords-to-position [{:keys [x y]}]
+(defn coords-to-position [x y]
   (str (int-to-column x)
        (inc y)))
 
-(defn available-moves [piece]
-  (let [position (:position piece)
-        {:keys [x y] :as coords} (position-to-coords position)]
-    (when (= :P (get-in piece [:value :type]))
-      (if (= "white" (get-in piece [:value :color]))
-        (map coords-to-position [{:y (inc y) :x x}])
-        (map coords-to-position [{:y (dec y) :x x}])))))
+(defn piece-to-coords [piece]
+  (-> piece :position position-to-coords))
 
+(defmulti available-moves #(get-in % [:value :type]))
+
+(defmethod available-moves :P [pawn]
+  (let [{:keys [x y]} (piece-to-coords pawn)]
+    (if (= "white" (get-in pawn [:value :color]))
+      (vector (coords-to-position x (inc y)))
+      (vector (coords-to-position x (dec y))))))
 
 ;;;;;;;;;;;;;
 (comment
