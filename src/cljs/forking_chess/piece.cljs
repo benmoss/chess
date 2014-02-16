6c287c23fe74f65e95cd64b7471975e8c35d9d4e
Ben Moss <ben@mossity.com>
2014-02-16 13:44:10 -0500

    Prevent available-targets from returning invalid positions

--- src/cljs/forking_chess/piece.cljs
+++ src/cljs/forking_chess/piece.cljs
@@ -1,23 +1,23 @@
 (ns forking-chess.piece
   (:require [clojure.set :refer [map-invert]]))
 
-(def board-size 8)
-
 (def column-to-int
   (zipmap (seq "abcdefgh")
-          (range)))
+          (range 8)))
 
 (def int-to-column
   (map-invert column-to-int))
-
 
 (defn position-to-coords [position]
   {:x (-> position first column-to-int)
    :y (-> position last str js/parseInt dec)})
 
 (defn coords-to-position [x y]
-  (str (int-to-column x)
-       (inc y)))
+  (let [column (int-to-column x)
+        row (inc y)]
+    (when (and column (>= y 0) (< y 8))
+      (str column
+           (inc y)))))
 
 (defn piece-to-coords [piece]
   (-> piece :position position-to-coords))
@@ -25,26 +25,31 @@
 (defmulti available-targets #(get-in % [:value :type]))
 
 (defmethod available-targets :P [pawn]
-  (let [{:keys [x y]} (piece-to-coords pawn)]
-    (if (= "white" (get-in pawn [:value :color]))
-      #{(coords-to-position x (inc y))}
-      #{(coords-to-position x (dec y))})))
+  (let [{:keys [x y]} (piece-to-coords pawn)
+        color (get-in pawn [:value :color])
+        op ({"white" inc "black" dec} color)]
+    (if-let [move (coords-to-position x (op y))]
+      #{move}
+      #{})))
 
 (defmethod available-targets :N [knight]
-  (let [{:keys [x y]} (piece-to-coords knight)]
-    (into #{} (for [op1 [+ -]
-                    op2 [+ -]
-                    xmove [1 2]
-                    ymove [1 2]
-                    :when (not= xmove ymove)]
-                (coords-to-position (op1 x xmove) (op2 y ymove))))))
+  (let [{:keys [x y]} (piece-to-coords knight)
+        ops [+ -]
+        moves [1 2]]
+    (into #{}
+          (remove nil?
+                  (for [op1 ops
+                        op2 ops
+                        xmove moves
+                        ymove moves
+                        :when (not= xmove ymove)]
+                    (coords-to-position (op1 x xmove) (op2 y ymove)))))))
 
 (defmethod available-targets :default [_] #{})
 
 ;;;;;;;;;;;;;
 (comment
-  (def white {:position "h2" :value {:color "white" :type :P}})
-  (def black {:position "a7" :value {:color "black" :type :P}})
   (available-targets {:position "h2" :value {:color "white" :type :N}})
-  (available-targets white)
-  (available-targets black))
+  (available-targets {:position "a7" :value {:color "black" :type :P}})
+  (available-targets {:position "h2" :value {:color "white" :type :P}})
+  (available-targets {:position "h1" :value {:color "black" :type :P}}))
